d3befa0d3b701e3ec48092e92a8f789df520b486
ValentinVPK <valentinvpk@abv.bg>
2025-04-12 17:41:30 +0300

    Test validate user ops

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -61,8 +61,6 @@ contract MinimalAccountTest is Test {
         // Assert
     }
 
-    // function testValidationOfUserOps() public {}
-
     function testRecoverIsngedOp() public {
         // Arrange
         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
@@ -82,4 +80,34 @@ contract MinimalAccountTest is Test {
         // Assert
         assertEq(actualSigner, minimalAccount.owner());
     }
+
+    // 1. Sign user ops
+    // 2. Call validate userops
+    // 3. Assert the return is correct
+    function testValidationOfUserOps() public {
+        // Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        bytes memory executeCallData =
+            abi.encodeWithSelector(MinimalAccount.execute.selector, dest, value, functionData);
+
+        PackedUserOperation memory packedUserOp =
+            sendPackedUserOp.generateSignedUserOperation(executeCallData, helperConfig.getConfig());
+
+        bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+
+        uint256 missingAccountFunds = 1e18;
+
+        // Act
+        vm.prank(helperConfig.getConfig().entryPoint);
+        uint256 validationData = minimalAccount.validateUserOp(packedUserOp, userOperationHash, missingAccountFunds);
+
+        // Assert
+        assertEq(validationData, 0);
+    }
+
+    // function testEntryPointCanExecuteCommands() public {}
 }
